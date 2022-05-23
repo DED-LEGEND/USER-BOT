dede552939453f27a22079bf66a08bb5de1aa095
Aditya Bots <105906694+adityabots@users.noreply.github.com>
2022-05-23 13:36:57 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -27,14 +27,14 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
 API_ID = int(getenv("API_ID", "12380656"))
 ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/1d6f55f754c0ad3b69048.jpg")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 DATABASE_URL = getenv("DATABASE_URL", "XXXXX")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", "XXXXX")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", "XXXXX")
-LOGS_GROUP_ID = getenv("LOGS_GROUP_ID", "XXXXX")
 MONGO_DB_URL = getenv("MONGO_DB_URL", "XXXXX")
 STRING_SESSION = getenv("STRING_SESSION", "session")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+LOGS_GROUP_ID = getenv("LOGS_GROUP_ID", "XXXXX")
 
 if LOGS_GROUP_ID:
     Owner = LOGS_GROUP_ID
